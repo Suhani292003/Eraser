97f44800c8254f2a5eb995ea4514b0e901ebbe2e
Suhani292003 <suhaniasawara@gmail.com>
2024-07-09 23:58:38 +0530

    team page added

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,37 @@
 "use client"
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import React, { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
+import { useConvex, useMutation, useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
 function Dashboard() {
+
+  const convex = useConvex();
+  const {user}:any=useKindeBrowserClient();
+  // const getUser=useQuery(api.user.getUser,{email:user?.email});
+
+  const createUser=useMutation(api.user.createUser);
+  useEffect(()=>{
+    if(user)
+    {
+     checkUser()
+    }
+  },[user])
+
+  const checkUser = async()=>{
+    const result=await convex.query(api.user.getUser,{email:user?.email});
+    if(!result?.length)
+      {
+      createUser({
+        name:user.given_name,
+        email:user.email,
+        image:user.picture
+      }).then((resp)=>{
+        console.log(resp)
+      })
+    }
+  }
   return (
     <div>Dashboard
         <Button>
